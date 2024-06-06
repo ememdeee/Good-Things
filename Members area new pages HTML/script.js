4b7876de26c456a80bddbd3af2364a6d9a60100b
Muhammad <muhammad@empirepixel.com>
2024-06-06 21:11:28 +0700

    memebrs area html

--- Members area new pages HTML/script.js
+++ Members area new pages HTML/script.js
@@ -12,4 +12,31 @@ document.addEventListener('DOMContentLoaded', function () {
     hamburgerIcon.addEventListener('click', toggleSidebar);
     closeIcon.addEventListener('click', toggleSidebar);
     overlay.addEventListener('click', toggleSidebar);
+
+// dropdown
+document.getElementById('dataSelected').addEventListener('click', function() {
+    document.getElementById('selectData').classList.toggle('show');
 });
+
+document.querySelectorAll('.dropdown-options div').forEach(function(option) {
+    option.addEventListener('click', function() {
+        var logoText = document.querySelector('.dropdown-selected .logo-text');
+        if (logoText) {
+            logoText.innerHTML = this.innerHTML;
+            document.getElementById('selectData').classList.remove('show');
+        } else {
+            console.error('Logo text element not found');
+        }
+    });
+});
+
+window.addEventListener('click', function(e) {
+    if (!e.target.closest('.custom-dropdown')) {
+        var dropdowns = document.querySelectorAll('.dropdown-options');
+        dropdowns.forEach(function(dropdown) {
+            dropdown.classList.remove('show');
+        });
+    }
+});
+});
+
