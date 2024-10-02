366a367b96d1d8526a11dee75480e40bf2446729
Muhammad <muhammad@empirepixel.com>
2024-10-02 22:17:35 +0700

    reallt-safe-replace-url-by-ahsan-and-me

--- WP PHP Codes/replaceUrl.sql
+++ WP PHP Codes/replaceUrl.sql
@@ -1,4 +1,69 @@
--- Final
+-- Note: 1. the table that need updated is both wp_posts[post_content] and wp_postmeta[elementor_data]
+-- to check manual
+SELECT * FROM `ns_3_posts` WHERE ID = 228768 
+SELECT * FROM `ns_3_postmeta` WHERE post_id = 223480 AND meta_key = '_elementor_data'
+
+-- from GPT
+-- Replace regular URLs in ns_3_posts
+UPDATE ns_3_posts 
+SET post_content = REPLACE(post_content, 'https://detailedvehiclehistory.com/vin-number', 'https://detailedvehiclehistory.com/vin-decoder')
+WHERE post_content LIKE '%https://detailedvehiclehistory.com/vin-number%';
+
+-- Replace regular URLs in ns_3_postmeta
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, 'https://detailedvehiclehistory.com/vin-number', 'https://detailedvehiclehistory.com/vin-decoder')
+WHERE meta_value LIKE '%https://detailedvehiclehistory.com/vin-number%';
+
+-- Replace escaped URLs in ns_3_postmeta
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, 'https:\/\/detailedvehiclehistory.com\/vin-number', 'https:\/\/detailedvehiclehistory.com\/vin-decoder')
+WHERE meta_value LIKE '%https:\/\/detailedvehiclehistory.com\/vin-number%';
+
+-- Replace Unicode-encoded URLs in ns_3_postmeta
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, '\u0022https:\/\/detailedvehiclehistory.com\/vin-number\u0022', '\u0022https:\/\/detailedvehiclehistory.com\/vin-decoder\u0022')
+WHERE meta_value LIKE '%\u0022https:\/\/detailedvehiclehistory.com\/vin-number\u0022%';
+
+-- Replace HTML-encoded double quotes around URLs in ns_3_postmeta
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, '&quot;https:\/\/detailedvehiclehistory.com\/vin-number&quot;', '&quot;https:\/\/detailedvehiclehistory.com\/vin-decoder&quot;')
+WHERE meta_value LIKE '%&quot;https:\/\/detailedvehiclehistory.com\/vin-number&quot;%';
+
+
+-- To 1 page only
+-- Replace regular URLs in ns_3_posts for page ID 223363
+UPDATE ns_3_posts 
+SET post_content = REPLACE(post_content, 'https://detailedvehiclehistory.com/vin-number', 'https://detailedvehiclehistory.com/vin-decoder')
+WHERE post_content LIKE '%https://detailedvehiclehistory.com/vin-number%'
+AND ID = 223363;
+
+-- Replace regular URLs in ns_3_postmeta for page ID 223363
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, 'https://detailedvehiclehistory.com/vin-number', 'https://detailedvehiclehistory.com/vin-decoder')
+WHERE meta_value LIKE '%https://detailedvehiclehistory.com/vin-number%'
+AND post_id = 223363;
+
+-- Replace escaped URLs in ns_3_postmeta for page ID 223363
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, 'https:\/\/detailedvehiclehistory.com\/vin-number', 'https:\/\/detailedvehiclehistory.com\/vin-decoder')
+WHERE meta_value LIKE '%https:\/\/detailedvehiclehistory.com\/vin-number%'
+AND post_id = 223363;
+
+-- Replace Unicode-encoded URLs in ns_3_postmeta for page ID 223363
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, '\u0022https:\/\/detailedvehiclehistory.com\/vin-number\u0022', '\u0022https:\/\/detailedvehiclehistory.com\/vin-decoder\u0022')
+WHERE meta_value LIKE '%\u0022https:\/\/detailedvehiclehistory.com\/vin-number\u0022%'
+AND post_id = 223363;
+
+-- Replace HTML-encoded double quotes around URLs in ns_3_postmeta for page ID 223363
+UPDATE ns_3_postmeta 
+SET meta_value = REPLACE(meta_value, '&quot;https:\/\/detailedvehiclehistory.com\/vin-number&quot;', '&quot;https:\/\/detailedvehiclehistory.com\/vin-decoder&quot;')
+WHERE meta_value LIKE '%&quot;https:\/\/detailedvehiclehistory.com\/vin-number&quot;%'
+AND post_id = 223363;
+
+
+
+-- Final with ahsan
 UPDATE ns_3_posts SET guid = replace(guid, 'https://detailedvehiclehistory.com/vin-number','https://detailedvehiclehistory.com/vin-decoder') LIMIT 200000;
 
 UPDATE ns_3_posts SET post_content = replace(post_content, 'https://detailedvehiclehistory.com/vin-number', 'https://detailedvehiclehistory.com/vin-decoder') LIMIT 200000;
@@ -9,7 +74,20 @@ WHERE meta_id > 60000
 LIMIT 60000;
 
 
+UPDATE ns_3_postmeta 
+SET meta_value = replace(meta_value, '\u0022https:\/\/detailedvehiclehistory.com\/vin-number\u0022', '\u0022https:\/\/detailedvehiclehistory.com\/vin-decoder\u0022') 
+WHERE post_id = 223363
 
+
+
+SELECT meta_value
+FROM ns_3_postmeta
+WHERE post_id = 223363
+AND meta_value LIKE 'https:\/\/detailedvehiclehistory.com\/vin-number\';
+
+
+
+-- 223368
 
 
 
