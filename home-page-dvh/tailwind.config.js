6c2f0eafc25e7ccbcf51d5d1d3f1ff456124689c
Muhammad <muhammad@empirepixel.com>
2024-11-18 23:02:23 +0700

    feedback v3 in progress

--- home-page-dvh/tailwind.config.js
+++ home-page-dvh/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",  // This will include all HTML files in the root directory
-    "./src/**/*.{js,ts,jsx,tsx}", // Include any JavaScript files if you have them
+    "./*.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+        'serif': ['Poppins', 'serif'],
+        'mono': ['Poppins', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
