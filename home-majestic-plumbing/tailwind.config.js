2eea0343a337d2eccba818f3482bd2f1a0f67dd1
ememdeee <basurah.muhammad.mb@gmail.com>
2025-07-19 09:57:09 +0700

    update

---
+++ home-majestic-plumbing/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}", "*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+      extend: {
+        // Your custom theme extensions
+      },
+    },
+    plugins: [],
+  }
